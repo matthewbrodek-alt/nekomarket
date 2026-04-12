7f37bc9aecba831c48d2745b0fcaadf27ced830e
matthewbrodek-alt <matthewbrodek@gmail.com>
2026-04-12 19:47:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Дизайн Nekomarket отрисован с точностью 100%');
-});
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      "colors": {
+          "primary": "#94464f",
+          "secondary": "#5f5e5e",
+          "background": "#f9f9f9",
+          "surface-container": "#eeeeee",
+          "surface-container-low": "#f3f3f3",
+          "on-surface": "#1a1c1c",
+          "outline-variant": "#d9c1c2",
+          "primary-container": "#dd828a",
+      },
+      "fontFamily": {
+          "headline": ["Plus Jakarta Sans"],
+          "body": ["Inter"],
+          "montserrat": ["Montserrat"]
+      }
+    }
+  }
+}
